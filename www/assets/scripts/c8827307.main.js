339445460ce359553eea983c628e6b7452e74e3f
Bobby Santiago <bobby.santiago@gmail.com>
2014-04-07 10:27:07 -0700

    [#68921944] Center home page map popup video play link icon

--- www/assets/scripts/c8827307.main.js
+++ www/assets/scripts/c8827307.main.js
@@ -10,7 +10,8 @@
         return $scope.showModal = !1, $scope.videoIframe = "", $scope.showSubscribeForm = !1, 
         $scope.location = $location, $rootScope.locationUrl = function() {
             return encodeURIComponent($location.absUrl());
-        }, "/articles" === $location.url() && ($scope.blogOverview = !0), "/thank_you" === $location.url() ? void ($scope.showThanks = !0) : ($timeout(function() {
+        }, "/articles" === $location.url() && ($scope.blogOverview = !0), "/thank_you" === $location.url() ? ($scope.showThanks = !0, 
+        void 0) : ($timeout(function() {
             var videoUrl;
             videoUrl = $location.search().video, null != videoUrl && ($scope.showModal = !0, 
             displayModal("http://www.youtube.com/watch?v=" + videoUrl));
@@ -1100,7 +1101,7 @@
                     pattern.setAttribute("width", "30"), pattern.setAttribute("height", "30"), image = document.createElementNS(svgNS, "image"), 
                     image.setAttribute("x", "0"), image.setAttribute("y", "0"), image.setAttribute("width", "24"), 
                     image.setAttribute("height", "24"), image.setAttributeNS(svgNSXLink, "xlink:href", url), 
-                    svgMap.appendChild(pattern), void pattern.appendChild(image);
+                    svgMap.appendChild(pattern), pattern.appendChild(image), void 0;
                 }, generateMap = function() {
                     var icon, markerList, wolrd_map, _i, _len, _ref;
                     for (markerList = null != scope.markers ? scope.markers : {
