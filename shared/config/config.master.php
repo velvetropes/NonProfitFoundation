c97be3ebc8dd3a864925493bec8480f9f9f512ad
Kara Todd <kara@todds.us>
2014-04-17 21:35:00 -0400

    [#69720030] - patch attempt

--- shared/config/config.master.php
+++ shared/config/config.master.php
@@ -127,6 +127,9 @@ if (isset($config))
 	// Session settings
 	$env_config['user_session_ttl'] = '702000';
 	$env_config['cp_session_ttl'] = '702000';
+	$env_config['cookie_prefix'] = 'starkeyf_'.ENV;
+	$env_config['require_ip_for_login']     = 'n';
+	$env_config['require_ip_for_posting']   = 'n';
 
 	/**
 	 * Custom upload directory paths
